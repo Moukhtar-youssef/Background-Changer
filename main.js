3cac4ba293c553bb2e7ccb1616ea4b766bf0a895
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2022-06-20 22:51:00 +0400

    making the code cleaner

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // start general variables
 input_1 = document.getElementById('1');
 input_2 = document.getElementById('2');
-color_Text = document.getElementById('text');
 direction = document.getElementById('direction');
 // end general variables
 // start reset variables
@@ -11,23 +10,28 @@ reset_color_1 = localStorage.getItem("reset");
 reset_color_2 = localStorage.getItem("reset_2");
 // end reset variables
 
+//checking every one millisecond
 setInterval(()=>{
             document.body.style.backgroundImage = `linear-gradient(${direction.value} ,${input_1.value},${input_2.value})`;
             color_Text.innerHTML =`linear-gradient ${direction.value} ,${input_1.value},${input_2.value}`
         },1);
+// set favorite color
 function favourite_color(){
     localStorage.setItem("value" , `${input_1.value}`);
     localStorage.setItem("value_2" , `${input_2.value}`);
+    localStorage.setItem("direction",`${direction.value}`);
     document.body.style.backgroundImage = `linear-gradient(${direction.value} ,${input_1.value},${input_2.value})`;
 }
+// get favorite color
 function display_favourite(){
-    favourite_color_1 = localStorage.getItem("value");
-    favourite_color_2 = localStorage.getItem("value_2");
-    input_1.value=`${favourite_color_1}`;
-    input_2.value=`${favourite_color_2}`;
-    document.body.style.backgroundImage = `linear-gradient(${direction.value} ,${favourite_color_1},${favourite_color_2})`;
+    input_1.value= localStorage.getItem("value");
+    input_2.value= localStorage.getItem("value_2");
+    direction.value = localStorage.getItem("direction");
+    document.body.style.backgroundImage = `linear-gradient(${direction.value} ,${input_1.value},${input_2.value})`;
 }
+// reset the colors to basic color
 function reset(){
     input_1.value=`${reset_color_1}`;
     input_2.value= `${reset_color_2}`;
+    direction.value = "to right";
 }
